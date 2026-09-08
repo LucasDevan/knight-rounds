9426dfa9bef2a51560fff84313c741dde2d488d4
retsag99 <lucas.devanlay@etu.univ-lyon1.fr>
2026-09-08 12:11:14 +0200

    stream lined testing inputted values and added more explanations

--- TourDuCavalier.py
+++ TourDuCavalier.py
@@ -1,16 +1,15 @@
 knightsMovements=[(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)] #Liste des mouvements possibles du cavalier en coordonnées relatives.
 continue_to_play=True #Booléen contrôlant la répétition du programme.
 
-def initialiseBoard(board:list)->list:
+def initialiseBoard(board:list, boardSize:int)->list:
     """_summary_
         Initialise un échiquier vide en remplissant une liste avec des zéros.
     Args:
         board (list): liste vide qui représentera l'échiquier
     """
-    for row in range(size):
-        board.append([0]*size)
-                
-                
+    for row in range(boardSize):
+        board.append([0]*boardSize*boardSize)
+
 def numberOfSpaceCanMoveTo(startingColumn:int,startingRow:int,boardSize:int)->int:
     """_summary_
     Calcule le nombre de cases valides accessibles depuis une position donnée.
@@ -30,7 +29,6 @@ def numberOfSpaceCanMoveTo(startingColumn:int,startingRow:int,boardSize:int)->in
         if(currentColumn<boardSize and currentColumn>=0 and currentRow>=0 and currentRow<boardSize):
             countPossibleMovement+=1
     return countPossibleMovement
-
 
 def hamiltonGraphMaker(board:list)->list:
     """_summary_
@@ -124,9 +122,9 @@ def solving(boardSize:int,listOfMoves:list,hamiltongraph:list,column:int,row:int
     priorityMoves=getPriorityMoves(column,row,hamiltongraph,boardSize)
     for move in priorityMoves:
         if(move[0]>=0):
-            res=solving(boardSize,listOfMoves,hamiltongraph,move[1],move[2])
-            if(res[0]):
-                return (True,res[1])
+            result = solving(boardSize,listOfMoves,hamiltongraph,move[1],move[2])
+            if(result[0]):
+                return result
             listOfMoves.remove((move[1],move[2]))
             updatingHamiltonGraph(move[1],move[2],hamiltongraph,False)
     return (False,[])
@@ -170,51 +168,81 @@ def printBoard(board:list):
             print("-",end="")
         print("")
 
-print("Bienvenue dans le jeu du tour du cavalier!",end="")
-print("Dans ce jeu, un cavalier doit parcourir toutes les cases d'un échiquier sans repasser par la même deux fois!",end="")
-print("Ce programme va automatiquement résoudre ce tour avant d'afficher la liste des mouvements effectués par le cavalier")
-while(continue_to_play):
-    try:
-        size=input("Rentrer la taille d'échiquier que le cavalier va parcourir: ") #Taille de l'échiquier
-        size = int(size)
-    except:
-        size='e' #valeur en cas d'exception 
-    try:
-        colonne=input(f"Rentrer la position de base du cavalier sur les colonnes de 0 à {size-1} (le défaut sera 0): ") #Position initiale en colonne
-        colonne = int(colonne)
-        ligne=input(f"Rentrer la position de base du cavalier sur les lignes de 0 à{size-1} (le défaut sera 0): ") #Position initiale en ligne
-        ligne = int(ligne)
-    except:
-        size='e'#valeur en cas d'exception 
-        
-            
-    if(size=='e'): #teste si il y a eu une exception
-        print("Aille vous n'avez pas rentré un nombre entier strictement positif, veuillez réessayer!") 
-    elif(size==1):
-        print("La solution est: [(0,0)]") #Solution trivial
-    elif(size<=3):
-        print("La taille rentrée ne permet pas de résoudre le problème.") #Impossible à réssoudre donc aucun intéret a faire les calcules
-    else:
-        if(colonne<0 or colonne>=size): #test si la valeur rentrée est correct
-            colonne=0 
-        if(ligne<0 or ligne>=size): #test si la valeur rentrée est correct
-            ligne=0    
-        board = []
-        initialiseBoard(board)
-        hamiltongraph=hamiltonGraphMaker(board)
-        try:
-            res=solving(len(board),[],hamiltongraph,colonne,ligne)
-            copyList=res[1].copy() #La liste des coups est comprit dans res[1]
-            if(not res[0] or not solverChecker(copyList,len(board))):
-                print("pas de solution trouvez")
-            else:
-                print("Une des solutions est:",res[1]) #affiche la solution
-        except RecursionError:
-            print("La taille que vous avez rentrée est trop grande pour que Python puisse résoudre (max:31)")
+print("Bienvenue dans le jeu du tour du cavalier!")
+print("Dans ce jeu, un cavalier doit parcourir toutes les cases d'un échiquier sans repasser par la même deux fois!")
+print("Ce programme va automatiquement résoudre ce tour avant d'afficher la liste des mouvements effectués par le cavalier"+'\n')
 
-    print("Voulez-vous recommencer?  (Oui ou Non)")
-    choix=str(input("")) #Permet de rejouer ou quitter le programme
-    if(choix.lower()=="non"):
-        continue_to_play=False
+INVALIDE_SIZE_VALUE : int = -1
+MIN_BOARD_SIZE : int = 1
+UNSOLVABLE_SIZES : list = [2,3]
+MAX_BOARD_SIZE : int = 31
+
+DEFAULT_POSITION : int = 0
+
+boardSizeInput : str
+rowPositionInput : str
+lignePositionInput : str
+
+boardSize : int
+rowPosition : int
+lignePosition : int
+
+board : list
+
+while(continue_to_play):
+    rowPosition = INVALIDE_SIZE_VALUE # reset the values on start
+    lignePosition = INVALIDE_SIZE_VALUE # reset the values on start
+
+    try:
+        boardSizeInput = input("Rentrer la taille de l'échiquier que le cavalier va parcourir (par exemple: 5 pour 5X5): ") #Taille de l'échiquier
+        boardSize = int(boardSizeInput)
+    except:
+        boardSize = INVALIDE_SIZE_VALUE #valeur en cas d'exception 
+
+    print("")
+    if(boardSize == INVALIDE_SIZE_VALUE or boardSize < MIN_BOARD_SIZE): #teste si il y a eu une exception
+        print("Aille vous n'avez pas rentré un nombre entier strictement positif, veuillez réessayer!") 
+    elif(boardSize == MIN_BOARD_SIZE):
+        print("La solution est: [(0,0)]") #Solution trivial
+    elif(boardSize in UNSOLVABLE_SIZES):
+        print("L'échiquier est trop petit pour résoudre le problème.") #Impossible à réssoudre donc aucun intéret a faire les calcules
+    elif(boardSize > MAX_BOARD_SIZE):
+        print(f"La taille d'échéquier que vous avez rentrée est trop grande pour que l'algorithme puisse le résoudre (max:{MAX_BOARD_SIZE})")
+    else:
+        while(rowPosition < 0 or rowPosition>=boardSize):#test si la valeur rentrée est correct
+            try:
+                rowPositionInput = input(f"Rentrer la colonne de 0 à {boardSize-1} sur laquelle le cavalier vas commencer son tour: ") #Position initiale en colonne
+                rowPosition = int(rowPositionInput)
+            except:
+                print("Vous avez rentré une valeur non conforme, la colonne de départ sera la ",DEFAULT_POSITION)
+                rowPosition = DEFAULT_POSITION
+
+        while(lignePosition < 0 or lignePosition>=boardSize):#test si la valeur rentrée est correct
+            try:
+                lignePositionInput = input(f"Rentrer la ligne de 0 à {boardSize-1} sur laquelle le cavalier va commencer son tour: ") #Position initiale en ligne
+                lignePosition = int(lignePositionInput)
+            except:
+                print("Vous avez rentré une valeur non conforme, la ligne de départ sera la ",DEFAULT_POSITION)
+                lignePosition = DEFAULT_POSITION
+        print("")
+        input(f"Tout est prêt! Appuyez sur Entrée pour générer la solution au problème du tour du cavalier sur un plateau de {boardSize} par {boardSize} avec la position de départ à la case ({rowPosition},{lignePosition}) ")
+
+        board = []
+        initialiseBoard(board,boardSize)
+        hamiltongraph = hamiltonGraphMaker(board)
+        try:
+            result = solving(len(board),[],hamiltongraph,rowPosition,lignePosition)
+            copyList = result[1].copy() #La liste des coups est comprit dans res[1]
+            if(not result[0] or not solverChecker(copyList,len(board))):
+                print("Aucune solution n'a été trouvée pour un échiquier de ",boardSize," par ",boardSize)
+            else:
+                print("Une des solutions est:",result[1]) #affiche la solution
+        except RecursionError:
+            print(f"La taille de l'échiquier est trop grande pour que l'algorithme puisse résoudre le tour du cavalier (max:{MAX_BOARD_SIZE})")
+
+    print("Voulez-vous recommencer?  (Tapper 'Non' pour quitter)")
+    choix=str(input("")).lower() #Permet de rejouer ou quitter le programme
+    if(choix=="non" or choix=="n" or choix=="no"):
+        continue_to_play = False
     
 print("Au revoire !")
